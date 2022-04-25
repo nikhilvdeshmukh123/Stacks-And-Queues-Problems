a8177dabaddcdf812e49183bdf21443aca132b91
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-26 05:09:48 +0530

    Added: Ability to Create Queue

---
+++ src/com/bridgelabz/StackQueue.java
@@ -0,0 +1,47 @@
+package com.bridgelabz;
+/*
+ * @author: Nikhil Deshmuhk
+ * Ability to create a Queue of 56->30->70
+ */
+import java.util.Scanner;
+
+public class StackQueue {
+	public static void main(String[] args) {
+		// Welcome Message for User
+		System.out.println("Welcome to Stacks and Queues Problems using Linked List.");
+
+		Scanner sc = new Scanner(System.in);
+		// Object of LinkedList class
+		LinkedList linkedList = new LinkedList();
+
+		int choice;
+		do {
+			System.out.println("STACK & QUEUE OPERATIONS");
+			System.out.println(
+					"1. PUSH in STACK \n2. POP from STACK \n3. DISPLAY STACK \n4. ENQUEUE in QUEUE \n5. DEQUEUE in QUEUE \n6. DISPLAY QUEUE \n7. EXIT \nEnter the Choice for Operation : ");
+			choice = sc.nextInt();
+
+			switch (choice) {
+            case 1:
+                System.out.println("Enter the Data to Push in Stack : ");
+                linkedList.insertFirst(sc.nextInt());
+                break;
+            case 2:
+                linkedList.peakPop();
+                break;
+            case 3:
+                linkedList.showLinkedList();
+                break;
+            case 4:
+                System.out.println("Enter the Data to Enqueue in Queue : ");
+                linkedList.insertLast(sc.nextInt());
+                break;
+            case 5:
+                break;
+            case 6:
+                linkedList.showLinkedList();
+                break;
+			}
+		} while (choice != 6);
+	}
+}
